5a992d16afa6be7ffd0b2309d8f652aee3404411
RubenNijhuis <contact@rubennijhuis.com>
2021-10-22 16:54:12 +0200

    small change

--- main.c
+++ main.c
@@ -8,14 +8,14 @@ int	main(void)
 {
 	int				fd;
 	unsigned int	i;
-	char			*string;
 
+	i = 0;
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
 	{
 		printf("Error! Could not open file\n");
 		exit(-1);
 	}
-	string = get_next_line(fd);
-	// printf("%s", string);
+	while (i < 8)
+		printf("%s", get_next_line(fd));
 }
